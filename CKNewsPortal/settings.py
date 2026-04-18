0b53aa126324f9067c66a392786adebc26d720cc
Salehin-07 <mdabusalehin123@gmail.com>
2026-04-18 07:24:14 +0600

    9th commit

--- CKNewsPortal/settings.py
+++ CKNewsPortal/settings.py
@@ -25,10 +25,21 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d$ul3c0s)o&53(8%@knw-)g8z1%l4@h6t-h7arht644)a9y3cc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
 DEBUG = False
 
-ALLOWED_HOSTS = ['cknewsportal.onrender.com','https://www.chatrokontho.pro.bd']
-CSRF_TRUSTED_ORIGINS = ['https://cknewsportal.onrender.com','https://www.chatrokontho.pro.bd/']
+ALLOWED_HOSTS = [
+    'cknewsportal.onrender.com',
+    'www.chatrokontho.pro.bd',
+    'chatrokontho.pro.bd',
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://cknewsportal.onrender.com',
+    'https://www.chatrokontho.pro.bd',
+    'https://chatrokontho.pro.bd',
+]
 
 
 # Application definition
